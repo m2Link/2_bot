46313fe64949e0fff1049a0d67c6eec3aee4b03c
m2Link <88938300+m2Link@users.noreply.github.com>
2021-08-19 00:02:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -22,11 +22,11 @@ And Start Searching
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/discussion_Group_2">m2Link</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/m2Link/2_bot">Click Me</a></i>
 """
